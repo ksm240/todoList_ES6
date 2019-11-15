66a487ca7b682c498f5818129c081af3e390062f
ksm240 <kankichi69@gmail.com>
2019-11-15 12:34:49 +0900

    Save todo into local strage

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const addButton = document.getElementById('addButton');
 const addInput = document.getElementById('itemInput');
 const todoList = document.getElementById('todoList');
-const listArray = [];
+let listArray = [];
 
 function listItemObj(content, status) {
   this.content = '';
@@ -10,12 +10,13 @@ function listItemObj(content, status) {
 
 const changeToComp = (e) => {
   const parent = e.target.parentElement;
+  console.log(parent);
   console.log('Changed to complete');
   parent.className = 'uncompleted well';
   e.target.innerText = 'Incomplete';
   e.target.removeEventListener('click', changeToComp);
   e.target.addEventListener('click',changeToInComp);
-  parent.firstChild.innerText = 'complete';
+  changeListArray(parent.firstChild.innerText, 'complete');
 }
 
 const changeToInComp = (e) => {
@@ -25,17 +26,37 @@ const changeToInComp = (e) => {
   e.target.innerText = 'Complete';
   e.target.removeEventListener('click', changeToInComp);
   e.target.addEventListener('click', changeToComp);
+  changeListArray(parent.firstChild.innerText, 'incomplete');
 }
 
 const removeItem = (e) => {
   const parent = e.target.parentElement.parentElement;
   parent.removeChild(e.target.parentElement);
   console.log(e.target.parentElement.firstChild.innerText);
+
+  const data = e.target.parentElement.firstChild.innerText;
+  for (let i=0; i < listArray.length; i++){
+    if(listArray[i].content == data){
+      listArray.splice(i,1);
+      refreshLocal();
+      break;
+    }
+  }
+}
+
+const changeListArray = (data, status) => {
+  for(let i=0; i < listArray.length; i++){
+    if(listArray[i].content == data){
+      listArray[i].status = status;
+      refreshLocal();
+      break;
+    }
+  }
 }
 
 const createItemDom = (text, status) => {
   const listItem = document.createElement('li');
-  const itemLabel =document.createElement('label');
+  const itemLabel = document.createElement('label');
   const itemCompBtn = document.createElement('button');
   const itemIncompBtn = document.createElement('button');
   listItem.className = (status == 'incomplete') ? 'complete well':'uncompleted well';
@@ -62,8 +83,19 @@ const createItemDom = (text, status) => {
   return listItem;
 }
 
+const refreshLocal = () => {
+  const todos = listArray;
+  localStorage.removeItem('todoList');
+  localStorage.setItem('todoList', JSON.stringify(todos));
+  console.log(listArray);
+}
+
 const addToList = () => {
+  const newItem = new listItemObj;
+  newItem.content = addInput.value;
   const item = createItemDom(addInput.value, 'incomplete');
+  listArray.push(newItem);
+  refreshLocal();
   todoList.appendChild(item);
   addInput.value ='';
 
@@ -71,6 +103,21 @@ const addToList = () => {
 
 const clearList = () => {
   todoList.innerHTML = '';
+}
+
+window.onload = () => {
+  const list = localStorage.getItem('todoList');
+  if (list != null) {
+    todos = JSON.parse(list);
+    listArray = todos;
+
+    for(let i=0; i<listArray.length; i++){
+      const data = listArray[i].content;
+      const item = createItemDom(data, listArray[i].status);
+      todoList.appendChild(item);
+    }
+  }
+  console.log(list);
 }
 
 addButton.addEventListener('click',addToList);
